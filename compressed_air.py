1051da025ffededd1f7bafbe564827c873da28df
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2020-10-28 15:45:54 +0500

    Update compressed_air.py

--- compressed_air.py
+++ compressed_air.py
@@ -12,14 +12,14 @@ st.title("Compressed Air Consumption")
 
 #subheader on sidebar
 
-st.sidebar.subheader("Change following slider values to see updated results in the slider on the right hand side:")
+st.subheader("Change following slider values to see updated results in the last slider at the bottom")
 
 #To create sliders on sidebar
 
-a = st.sidebar.slider('Weft Count (Ne)', 10, 80, 20)  # 👈 this is a widget
-b = st.sidebar.slider('Fabric Width (in)', 60, 120, 60)
-c = st.sidebar.slider('Loom RPM', 450, 650, 500)
-d = st.sidebar.slider('Reed (dents/in)', 15, 55, 30)
+a = st.slider('Weft Count (Ne)', 10, 80, 20)  # 👈 this is a widget
+b = st.slider('Fabric Width (in)', 60, 120, 60)
+c = st.slider('Loom RPM', 450, 650, 500)
+d = st.slider('Reed (dents/in)', 15, 55, 30)
 
 #Regression equation to calculate air consumption; int is used to convert float into integer; this is done because other arguments in slider are also int types
 e = int(6.10295 -(0.0169465 * a)+(0.0158989 * b)-(0.000147579 * c)+(0.0228664 * d)+(0.00114483 * b * b)+(0.000127147 * b * c)-(0.000849611 * b * d))
